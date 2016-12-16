293e33950fbda7534873abbb439cb135d91befb7
No Name <dfnoname@users.noreply.github.com>
2016-12-16 21:23:49 +0700

    Update generate-index.php

--- generate-index.php
+++ generate-index.php
@@ -1,14 +1,7 @@
 <?php
+error_reporting(0);
+@ini_set('display_errors', 0);
 date_default_timezone_set('Asia/Jakarta');
-
-if(!isset($_GET['index_file'])){
-	exit("index_file not set = 1 - 32");
-}
-if(empty($_GET['index_file'])){
-	exit("index_file empty = 1 - 32");
-}
-
-$INDEXFILES= $_GET['index_file'] - 1;
 
 $allfile= glob("KWKU/*.txt");
 
@@ -16,49 +9,18 @@ $allfile= glob("KWKU/*.txt");
 			exit("ZONK");
 		}
 
-$sfile= $allfile[$INDEXFILES];
+$sfile= $allfile[array_rand($allfile)];
 
 $arraysku= array_unique(array_filter(explode("\n", file_get_contents($sfile))));
 
-$DATES= date('c');
-
-$html='<HTML><head><title>WELCOME TO THE BEST OF EBOOK LIBRARY</title></head><body><a href="/sitemaps.xml">Sitemaps</a><br>';
-
-$sitemaps = '<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/css-sitemap-single.xsl"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$html='<HTML><head><title>WELCOME TO THE BEST OF EBOOK LIBRARY</title></head><body>';
 
 foreach($arraysku as $items){
 	$clean_title= preg_replace('![^a-z0-9]+!i', ' ', $items);
 	$slug= trim(str_replace(' ','-', $clean_title), '-');
 	
 		$html .= '<a href="/Storage/'.$slug.'.pdf">'.$clean_title.'</a><br>';
-		
-		$sitemaps .= '<url>
-			<loc>http://'.$_SERVER['SERVER_NAME'].'/Storage/'.$slug.'.pdf</loc>
-			<lastmod>'.$DATES.'</lastmod>
-			<changefreq>weekly</changefreq>
-			<priority>1</priority>
-			</url>';
 }
 
-$html .='<a href="/Storage/">View All Files</a></body></HTML>';
-
-$sitemaps .= '</urlset>';
-
-$ff= fopen("index.html", "w");
-fwrite($ff, $html);
-fclose($ff);
-
-$ffs= fopen("sitemaps.xml", "w");
-fwrite($ffs, $sitemaps);
-fclose($ffs);
-
-		//remove all file
-			foreach($allfile as $files){
-				if(@unlink($files)){
-					echo "sukses remove ".$files."<br>";
-				}
-			}
-
+$html .='</body></HTML>';
 ?>
